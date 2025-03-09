93120c19304d109d3e3c641c2d89cab4d088de57
Ammar324654 <140363685+Ammar324654@users.noreply.github.com>
2025-03-09 14:15:44 +1030

    Update Referee.cpp

--- Referee.cpp
+++ Referee.cpp
@@ -6,7 +6,7 @@ Player* Referee::refGame(Player* player1, Player* player2) {
     char move1 = player1->makeMove();
     char move2 = player2->makeMove();
     
-    if (move1 == move2) return nullptr; // Tie
+    if (move1 == move2) return nullptr; 
     
     if ((move1 == 'R' && move2 == 'S') || 
         (move1 == 'S' && move2 == 'P') || 
